b48763fb0841627df49cdef5d5ec1103c696d488
Shaj2311 <haideralijafri2007@gmail.com>
2025-11-08 16:02:09 +0500

    Draw player via carDesign mem label instead of manual printing

--- player.asm
+++ player.asm
@@ -30,12 +30,13 @@ drawPlayer:
 push bp
 mov bp, sp
 pusha
+push es
+push ds
 
+
+	;point es:di to player position
 	mov ax, 0xb800
 	mov es, ax
-
-
-	;point di to player position
 	mov ax, [playerX]	;x position
 	sub ax, 2		;offset by 2 locations to center car
 	push ax
@@ -44,78 +45,28 @@ pusha
 	call pointToXY
 
 
+	;point ds:si to car design label
+	push cs
+	pop ds
+	mov si, carDesign
 
-	;draw car from memory
-	;draw car rectangle
-	mov ax, [carRectHeight]
-	push ax
-	mov ax, [carRectWidth]
-	push ax
-	mov ax, 0x4020
-	push ax
-	call drawRect
-
-	;print cool stuff
-	mov ah, 0x04
-	mov al, '/'
-	mov [es:di], ax
-	mov [es:di + 160*(5 - 1) + (12 - 2)], ax	;(height-1) spaces down, (width-1) spaces across
-	
-	mov al, '\'
-	mov [es:di + (12 - 2)], ax
-	mov [es:di + 160*(5 - 1)], ax			;(height-1) spaces down
+	;print player 
+	mov cx, [carHeight]
+	printPlayerLoopOuter:
+	push di
+	push cx
+		;draw a row
+		mov cx, [carWidth]
+		rep movsw
+	pop cx
+	pop di 
+	add di, 160
+	loop printPlayerLoopOuter
 
 	
 
-	;draw cool smol rectangle
-	push di
-		add di, 160
-		add di, 160
-		add di, 2
-		add di, 2
-		mov ax, [carRectHeight]
-		sub ax, 4
-		push ax
-		mov ax, [carRectWidth]
-		sub ax, 4
-		push ax
-		mov ah, 0x04
-		mov al, ' '
-		push ax
-		call drawRect
-	pop di
-
-	;draw cool wheels
-	push di
-	mov cx, 0x08DB
-	mov [es:di + 160 - 2], cx
-
-	push di
-		mov ax, [carRectWidth]	;get width of car
-		shl ax, 1		;multiply by 2 (convert to byte offset)
-		add di, ax
-		mov [es:di + 160], cx
-	pop di
-
-	push di
-		mov ax, [carRectHeight]	;get height of car
-		sub ax, 2		;both bumpers
-		mov bx, 160		;multiply by 160
-		mul bx
-		add di, ax
-		mov [es:di - 2], cx
-		mov ax, [carRectWidth]
-		shl ax, 1
-		add di, ax
-		mov [es:di], cx
-	pop di
-	;mov [es:di + 160 + 2*4] dx
-	pop di
-
-
-
-
-
+pop ds
+pop es
 popa
 pop bp
 ret
@@ -123,32 +74,6 @@ ret
 
 
 
-
-
-;drawRect(height, width)
-drawRect:
-push bp
-mov bp, sp
-pusha
-	
-	;print rectangle
-	mov ax, [bp + 4]
-	mov cx, [bp + 6]
-	rectOuter:
-	push cx
-		mov cx, [bp + 8]
-		push di
-		rectInner:
-			mov [es:di], ax
-			add di, 160
-		loop rectInner
-		pop di
-	add di, 2
-	pop cx
-	loop rectOuter
-popa
-pop bp
-ret 6
 ;=========== FUNCTION END: initPlayer() HELPERS: drawPlayer(X,Y) ==============
 
 
